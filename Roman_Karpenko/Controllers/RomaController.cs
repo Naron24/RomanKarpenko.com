832c3ba03e32144eaa40015428b9da67532fa5ab
Micron <ankarpo@gmail.com>
2018-06-14 21:22:04 +0300

    Add project files.

---
+++ Roman_Karpenko/Controllers/RomaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roman_Karpenko.Controllers
+{
+    public class RomaController : Controller
+    {
+        // GET: Roma
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Roman_Karpenko/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Roman_Karpenko.MvcApplication" Language="C#" %>
